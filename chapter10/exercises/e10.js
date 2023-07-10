ab2fb4fddf7ede65ac6be31b8f634058c097493f
Dahyun Kim <DahyunClaireKim@Dahyuns-MacBook-Air-2.local>
2023-07-10 10:38:56 -0500

    testing1

--- chapter10/exercises/e10.js
+++ chapter10/exercises/e10.js
@@ -1,11 +1,11 @@
 // 10.10.1. Rectangles Code Here
 
-
+console.log("Hello");
+console.log("Claire");
+console.log("Thank you Kimberly")
 
 
 // 10.10.2. Triangles Code Here
-
-
 
 
 // 10.10.3. Diamonds Code Here
